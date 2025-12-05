fd2da0c44a782d06bd191aae77a69f5e11355ce2
Oday Darweesh <odaydarweesh@gmail.com>
2025-12-05 14:17:26 +0100

    Add Dockerfile for BookQuoteApi

--- backend/BookQuoteApi/obj/Debug/net9.0/BookQuoteApi.AssemblyInfo.cs
+++ backend/BookQuoteApi/obj/Debug/net9.0/BookQuoteApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookQuoteApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1bf13e97102a995075d564e893c9be6662443da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36eb7d85b81a8739a2a66a8f8642a51ec91b5d8e")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookQuoteApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookQuoteApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
