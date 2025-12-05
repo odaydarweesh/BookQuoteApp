e830f1aa55e6cb35a0938550d0dd3d1ed19da966
Oday Darweesh <odaydarweesh@gmail.com>
2025-12-05 14:57:09 +0100

    Align packages to .NET 8

--- backend/BookQuoteApi/obj/Debug/net8.0/BookQuoteApi.AssemblyInfo.cs
+++ backend/BookQuoteApi/obj/Debug/net8.0/BookQuoteApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookQuoteApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff976babdd9ded185595006a54413ab861bf0244")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cafbefce6fa35fe97a378cf7de1ff4dd3a663272")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookQuoteApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookQuoteApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
